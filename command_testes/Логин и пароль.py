a5d3ec2c0df78d4c380f0311a6dcc59b8c5f2690
JoeyTheRoach <69213870+JoeyTheRoach@users.noreply.github.com>
2022-07-23 19:42:09 +0500

    Add files via upload

---
+++ command_testes/Логин и пароль.py
@@ -0,0 +1,7 @@
+Login = input('Введи логин')
+Pass = input ('Введи пароль')
+if Login == 'Host12' and Pass == '25565' or Login == 'admin' and Pass == 'admin':
+    result = 'Хай'
+else:
+    result = 'нет'
+print(result)
