f40172af26cf5810ce24bd1cd81fb2333b3ba72c
JoeyTheRoach <69213870+JoeyTheRoach@users.noreply.github.com>
2022-07-24 17:56:06 +0500

    Add files via upload

---
+++ simple_games/Troubles with copy e-mail.py
@@ -0,0 +1,14 @@
+import pyperclip
+import time
+old = ''
+while True:
+    s = pyperclip.paste()
+    if(s == old):
+        time.sleep(3)
+    elif(s [-10:]) == ('@gmail.com') or (s [-8:]) == ('@mail.ru')
+        print('coolhacker@xakep.ru')
+        old = s
+    elif(s != old) and (s [:10]) != '@gmail.com' or (s [:9]) != '@mail.com':
+        print(s)
+        old = s
+        time.sleep(3)
