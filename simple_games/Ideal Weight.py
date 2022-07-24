e3e8efd7068f432e75318fa7a2c74e8487aea983
JoeyTheRoach <69213870+JoeyTheRoach@users.noreply.github.com>
2022-07-24 14:05:24 +0500

    Add files via upload

---
+++ simple_games/Ideal Weight.py
@@ -0,0 +1,10 @@
+while True:
+    h = int(input('Сколько в тебе роста?' ))
+    a = int(input('Сколько тебе лет?' ))
+    if a < 40:
+        w = h - 110
+
+    else:
+        w = h - 100
+    print(w)
+    
